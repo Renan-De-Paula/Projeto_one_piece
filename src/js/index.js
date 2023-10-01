446b24d1f5794267af4deede215fc7e4afa40738
Renan De Paula <119828475+Renan-De-Paula@users.noreply.github.com>
2023-10-01 16:43:12 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+const botoes = document.querySelectorAll(".botao");
+const botoes2 = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+	botao.addEventListener("click", () => {
+		desselecionarBotao();
+		desselecionarPersonagem();
+
+		botao.classList.add("selecionado");
+		personagens[indice].classList.add("selecionado");
+	});
+});
+botoes2.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+});
+
+function desselecionarPersonagem() {
+	const personagemSelecionado = document.querySelector(".personagem.selecionado");
+	personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+	const botaoSelecionado = document.querySelector(".botao.selecionado");
+	botaoSelecionado.classList.remove("selecionado");
+}
+
+
+window.alert("Ola, mundo!")
+window.alert("Eu me chamo Renan D. Paula e Serei o rei dos programadores")
+
